a98839a10eef86322a15e935ab42ffe08cfbe655
Hamza-Eng <hamzamadani217@gmail.com>
2023-10-13 16:56:01 +0100

    disable spring security & add H2database

--- src/main/java/com/gestion/ecole/gestionecole/configuration/WebSecurityConfiguration.java
+++ src/main/java/com/gestion/ecole/gestionecole/configuration/WebSecurityConfiguration.java
@@ -1,13 +1,9 @@
 package com.gestion.ecole.gestionecole.configuration;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-@Configuration
+//@Configuration
 public class WebSecurityConfiguration {
 
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
-    }
+//    public PasswordEncoder passwordEncoder(){
+//        return new BCryptPasswordEncoder();
+//    }
 }
