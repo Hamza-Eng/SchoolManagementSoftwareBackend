faf2a03b267436f1dc66e94ed11eeec1bd0d8e99
mourad01 <55068060+mourad01@users.noreply.github.com>
2023-10-14 16:21:58 +0100

    +

--- src/main/java/com/gestion/ecole/gestionecole/controller/EmployeController.java
+++ src/main/java/com/gestion/ecole/gestionecole/controller/EmployeController.java
@@ -6,39 +6,50 @@ import java.util.Optional;
 
 import com.gestion.ecole.gestionecole.entities.Employes;
 import com.gestion.ecole.gestionecole.utility.ControllerGenerator;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/employe")
 
 public class EmployeController implements ControllerGenerator<Employes> {
 
+	@PostMapping("/saveorupdate")
 	@Override
 	public Employes saveOrUpdate(Employes t) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	@GetMapping("/fingbyid{id} ")
 	@Override
-	public Optional<Employes> findById(Long id) {
+	public Optional<Employes> findById(@PathVariable Long id) {
 		// TODO Auto-generated method stub
 		return Optional.empty();
 	}
 
+	@GetMapping("/findbycriteria")
 	@Override
 	public Optional<Employes> findByCriteria(HashMap<String, String> map) {
 		// TODO Auto-generated method stub
 		return Optional.empty();
 	}
 
+	@DeleteMapping("/delete{id}")
 	@Override
 	public Boolean delete(Long id) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	@DeleteMapping("/deleteall")
 	@Override
 	public Boolean deleteAll() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	@GetMapping("/findall")
 	@Override
 	public List<Employes> findAll() {
 		// TODO Auto-generated method stub
--- src/main/java/com/gestion/ecole/gestionecole/controller/NiveauController.java
+++ src/main/java/com/gestion/ecole/gestionecole/controller/NiveauController.java
@@ -6,39 +6,46 @@ import java.util.Optional;
 
 import com.gestion.ecole.gestionecole.entities.Niveaux;
 import com.gestion.ecole.gestionecole.utility.ControllerGenerator;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
+@RestController
+@RequestMapping("/niveau")
 public class NiveauController implements ControllerGenerator<Niveaux> {
 
+	@PostMapping("/saveorupdate")
 	@Override
 	public Niveaux saveOrUpdate(Niveaux t) {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
+	@GetMapping("/fingbyid{id} ")
 	@Override
 	public Optional<Niveaux> findById(Long id) {
 		// TODO Auto-generated method stub
 		return Optional.empty();
 	}
 
+	@GetMapping("/findbycriteria")
 	@Override
 	public Optional<Niveaux> findByCriteria(HashMap<String, String> map) {
 		// TODO Auto-generated method stub
 		return Optional.empty();
 	}
-
+	@DeleteMapping("/delete{id}")
 	@Override
 	public Boolean delete(Long id) {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
+	@DeleteMapping("/deleteall")
 	@Override
 	public Boolean deleteAll() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	@GetMapping("/findall")
 	@Override
 	public List<Niveaux> findAll() {
 		// TODO Auto-generated method stub
